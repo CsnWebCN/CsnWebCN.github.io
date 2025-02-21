2f0493e3c531b14028c2c59a4686852451368def
carson1145141919810 <290401421@qq.com>
2025-02-21 19:12:09 +0800

    Site updated: 2025-02-21 19:12:08

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1739599695079";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1740136325444";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
